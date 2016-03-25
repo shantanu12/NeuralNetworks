d4b6e67f4dd7592d74dcd5878f9d7f292ac648f3
shantanu <shantanu88@tamu.edu>
2016-03-24 22:33:06 -0500

    Completed the project

--- src/Master.java
+++ src/Master.java
@@ -4,10 +4,10 @@ public class Master {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		String dataset = "tictactoe";
-		int hidden = Integer.parseInt("0");
-		int nodes = Integer.parseInt("2");
-		Double learningRate = Double.parseDouble("0.1");
+		String dataset = args[0];
+		int hidden = Integer.parseInt(args[1]);
+		int nodes = Integer.parseInt(args[2]);
+		Double learningRate = Double.parseDouble(args[3]);
 		FileHandler reader = new FileHandler();
 		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
 		ArrayList<Record> records = new ArrayList<Record>();
@@ -40,7 +40,13 @@ public class Master {
 			break;
 		}
 
+		System.out.println("# of hidden layers: " + hidden);
+		System.out.println("# of hidden nodes: " + nodes);
+		System.out.println("learning rate: " + learningRate);
+
 		int split = ipop.size() / 10;
+		ArrayList<Double> accuracy = new ArrayList<Double>();
+		ArrayList<Integer> epochs = new ArrayList<Integer>();
 		for (int i = 0; i < 10; i++) {
 			ArrayList<IOArray> buildData = new ArrayList<IOArray>();
 			ArrayList<IOArray> trainingData = new ArrayList<IOArray>();
@@ -99,6 +105,7 @@ public class Master {
 			ArrayList<Layer> bestNetwork = new ArrayList<Layer>();
 			Double mse = 0.0;
 			Double bestMSE = Double.MAX_VALUE;
+			int bestEpoch = 0;
 			int epoch = 1;
 			do {
 				for (IOArray data : trainingData) {
@@ -112,6 +119,7 @@ public class Master {
 				mse = error / validationData.size();
 				if (mse < bestMSE) {
 					bestMSE = mse;
+					bestEpoch = epoch;
 					bestNetwork = new ArrayList<Layer>();
 					for (int l = 0; l < network.size(); l++) {
 						bestNetwork.add(new Layer(network.get(l)));
@@ -132,13 +140,14 @@ public class Master {
 				mse = error / validationData.size();
 				if (mse < bestMSE) {
 					bestMSE = mse;
+					bestEpoch = epoch;
 					bestNetwork = new ArrayList<Layer>();
 					for (int l = 0; l < network.size(); l++) {
 						bestNetwork.add(new Layer(network.get(l)));
 					}
 				}
 				epoch++;
-			} while (epoch < 5000 && mse <= bestMSE);
+			} while (epoch <= 5000 && mse <= bestMSE);
 
 			int firstBreak = epoch;
 			while (epoch < 2 * firstBreak) {
@@ -153,6 +162,7 @@ public class Master {
 				mse = error / validationData.size();
 				if (mse < bestMSE) {
 					bestMSE = mse;
+					bestEpoch = epoch;
 					bestNetwork = new ArrayList<Layer>();
 					for (int l = 0; l < network.size(); l++) {
 						bestNetwork.add(new Layer(network.get(l)));
@@ -160,7 +170,9 @@ public class Master {
 				}
 				epoch++;
 			}
-
+			System.out.println("Iteration " + (i + 1));
+			System.out.println("	Epochs to convergence: " + bestEpoch);
+			epochs.add(bestEpoch);
 			// test the network
 			int passed = 0;
 			for (IOArray data : testingData) {
@@ -168,8 +180,11 @@ public class Master {
 					passed++;
 				}
 			}
-			Double accuracy = passed / (double) testingData.size();
-			System.out.println("Accuracy: " + accuracy);
+			Double correctness = Utils.round(passed / (double) testingData.size() * 100, 2);
+			accuracy.add(correctness);
+			System.out.println("	Accuracy: " + correctness);
 		}
+		System.out.println("----------OVERALL----------");
+		Utils.calculateStats(accuracy, epochs);
 	}
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Utils {
 	public static Double matrixMultiply(Double[] a, Double[] b) {
@@ -24,5 +25,43 @@ public class Utils {
 			}
 		}
 		return index;
+	}
+
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+
+		long factor = (long) Math.pow(10, places);
+		value = value * factor;
+		long tmp = Math.round(value);
+		return (double) tmp / factor;
+	}
+
+	public static void calculateStats(ArrayList<Double> accuracy, ArrayList<Integer> epochs) {
+		int epochSum = 0;
+		for (int i : epochs) {
+			epochSum += i;
+		}
+		int meanEpochs = epochSum / 10;
+		System.out.println("Mean Epochs to convergence: " + meanEpochs);
+		Double sumAccuracy = 0.0;
+		Double sumError = 0.0;
+		for (Double d : accuracy) {
+			sumAccuracy += d;
+			sumError += 100 - d;
+		}
+		Double meanAccuracy = round(sumAccuracy / 10, 2);
+		System.out.println("Mean Accuracy: " + meanAccuracy + "%");
+		Double meanError = round(sumError / 10, 2);
+		Double sum = 0.0;
+		for (Double d : accuracy) {
+			sum += Math.pow((100 - d - meanError), 2);
+		}
+		double standardDev = round(Math.sqrt(sum / 10), 2);
+		System.out.println("Standard Deviation: " + standardDev + "%");
+		double standardError = round(standardDev / Math.sqrt(10), 2);
+		System.out.println("Standard Error: " + standardError + "%");
+		double CIBound = round(2.23 * standardError, 2);
+		System.out.println("Confidence Interval: " + meanAccuracy + " +- " + CIBound);
 	}
 }
